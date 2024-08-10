6fb82af3e5d45dc8099291c4788060ab81ad825b
jasper0831 <lyinjue456852@gmail.com>
2024-08-10 17:48:16 +0800

    添加deploy到git page的指令

--- components.d.ts
+++ components.d.ts
@@ -8,7 +8,6 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppHeader: typeof import('./src/components/AppHeader.vue')['default']
-    HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
